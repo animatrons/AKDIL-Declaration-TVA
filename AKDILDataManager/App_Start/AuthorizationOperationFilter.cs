e6ec390cea308737f3a7db55bc6573357f44eab9
Amin Harwal <medamineharwel@outlook.com>
2021-06-03 16:13:49 +0100

    Add Swagger Documentation to API
    Adds Swashbuckle/Swagger to the API for ducumentation and testing.

---
+++ AKDILDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,32 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+// 
+// Adding a parameter to every operation that allow us put the token in 
+
+namespace AKDILDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null) // Get (no parameters)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
